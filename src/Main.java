7704550fbd69f0e21907c97776be18c4eeaca55b
s34979 <s34979@pjwstk.edu.pl>
2026-03-10 11:20:26 +0100

    Add helper information about the application

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class main
   public static void main(String[] args)
    {
      System.out.println("Hello from my first git project!");
+     System.out.println("Application information loaded.");
     }
 }
+
